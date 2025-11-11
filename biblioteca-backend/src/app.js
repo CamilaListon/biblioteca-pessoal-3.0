1e3da3e23569bcedd2ee804c478c6a8ca4faff20
GiuliaSouzaDaF <giuliasouzadafonsecasc@gmail.com>
2025-11-11 19:48:40 -0300

    Tokens,Hash,Server,Auth e Camila vai testar

--- biblioteca-backend/src/app.js
+++ biblioteca-backend/src/app.js
@@ -1,12 +1,24 @@
 import express from "express";
+import cors from "cors";
 import { usuarioRouter } from "./routes/usuarios.js";
 import { livroRouter } from "./routes/livros.js";
+import authRouter from "./routes/authRoutes.js";
+import { auth } from "./middleware/auth.js";
 
 
-const app = express();
+export const app = express();
+
+app.use(cors()); 
 app.use(express.json());
 
+app.get("/ping", (req, res) => {
+  console.log(" GET /ping chegou");
+  res.send("pong");
+});
+
+app.use('/auth', authRouter)
+
+app.use(auth);
 app.use(usuarioRouter);
 app.use(livroRouter);
 
-app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
